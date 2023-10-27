121bdd5a16d7d8f822a4915d583a7a31186be40c
Joanne Monaghan <joanne.monaghan@bbc.co.uk>
2023-10-27 08:57:45 +0100

    Fetching/parsing data tested, sign in and topic selection pages added

---
+++ miniSportApp/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  miniSportApp
+//
+//  Created by Joanne Monaghan on 19/10/2023.
+//
+
+import Foundation
---
+++ miniSportApp/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  networkManager.swift
+//  miniSportApp
+//
+//  Created by Joanne Monaghan on 19/10/2023.
+//
+
+import Foundation
